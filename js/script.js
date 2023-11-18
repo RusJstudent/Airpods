2e6e30570678811580d413bddb24e0e422bab369
Sergey RJS <sergeydes@yandex.ru>
2023-11-18 17:02:41 +0300

    First 3 parts (without JS) is done

---
+++ js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// code below for testing only
+// const blue = document.getElementById('blue');
+// const pink = document.getElementById('pink');
+
+// const styles = getComputedStyle(document.querySelector(':root'));
+
+// document.addEventListener('click', function(e) {
+//     if (e.target.tagName !== 'BUTTON') return;
+
+//     let buttonColor = e.target.textContent;
+
+//     let color = styles.getPropertyValue(`--${buttonColor}`);
+//     document.querySelector(':root').style.setProperty('--color', color);
+// })
