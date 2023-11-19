c0c7920fef267d3d571b76aaeab6438891363dca
Sergey RJS <sergeydes@yandex.ru>
2023-11-19 09:36:52 +0300

    Add highlight bg for M letter and finished project using JS

--- js/script.js
+++ js/script.js
@@ -1,16 +1,64 @@
 'use strict';
 
-// code below for testing only
-// const blue = document.getElementById('blue');
-// const pink = document.getElementById('pink');
+// Можно добавить loader чтобы нужный цвет успевал загружаться
 
-// const styles = getComputedStyle(document.querySelector(':root'));
+// data
+let defaultColor = 'blue'; // any color from availableColors
+let absentColor = 'blue'; // initially made styles for blue color, so this one is not among small images
+const availableColors = {
+    pink: '#F3D6D1',
+    blue: '#CADBEB',
+    black: '#B6B6B6',
+    green: '#DDEAD9',
+    white: '#FAFAFA',
+};
 
-// document.addEventListener('click', function(e) {
-//     if (e.target.tagName !== 'BUTTON') return;
+// initial styles
+document.querySelector(':root').style.setProperty('--color', availableColors[defaultColor]);
+const airpodsBig = document.querySelector('.main-screen__images img');
+const composition = document.querySelector('.composition__image img');
+const smartCase = document.querySelector('.battery__image img');
+const colourBody = document.querySelector('.colour__body');
+const smallImages = Array.from(colourBody.querySelectorAll('img[alt="airpods"]'));
 
-//     let buttonColor = e.target.textContent;
+changeColor();
 
-//     let color = styles.getPropertyValue(`--${buttonColor}`);
-//     document.querySelector(':root').style.setProperty('--color', color);
-// })
+// events
+colourBody.onmousedown = () => false;
+colourBody.onclick = function(e) {
+    if (e.target.alt !== "airpods") return;
+
+    const src = e.target.getAttribute('src');
+    defaultColor = getColor(src);
+    document.querySelector(':root').style.setProperty('--color', availableColors[defaultColor]);
+    
+    changeColor();
+}
+
+// functions
+function changeColor() {
+    [airpodsBig, composition, smartCase].forEach(img => {
+        const src = img.getAttribute('src');
+        const color = getColor(src);
+        const newSrc = src.replace(color, defaultColor);
+        img.setAttribute('src', newSrc);
+    });
+
+    smallImages.forEach(img => {
+        const src = img.getAttribute('src');
+        const color = getColor(src);
+        if (color !== defaultColor) return;
+        const newSrc = src.replace(color, absentColor);
+        img.setAttribute('src', newSrc);
+        absentColor = defaultColor;
+    });
+}
+
+function getColor(src) {
+    const regex = /\w+(?=\.)/;
+
+    const color = src.match(regex)?.[0];
+    if (!availableColors[color]) throw new Error('no such color defined');
+
+    return color;
+}
